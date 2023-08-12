649184ee0106f52d7968d8b4958f42fe3f070b13
Nick Thacke <ncthacke@gmail.com>
2023-08-12 10:21:48 -0400

    Initial Commit

---
+++ MovieRatingApp/MovieRatingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieRatingAppApp.swift
+//  MovieRatingApp
+//
+//  Created by Nick Thacke on 8/12/23.
+//
+
+import SwiftUI
+
+@main
+struct MovieRatingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

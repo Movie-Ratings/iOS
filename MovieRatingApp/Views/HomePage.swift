b54cb2cab17a51d83cc25437c9ebc1c6434a7b0d
Nick Thacke <ncthacke@gmail.com>
2023-08-15 18:09:41 -0400

    nicer frontEnd

--- MovieRatingApp/Views/HomePage.swift
+++ MovieRatingApp/Views/HomePage.swift
@@ -14,14 +14,41 @@ struct HomePage : View {
     @EnvironmentObject var manager : Manager
     
     var body : some View {
-        ScrollView {
-            Spacer()
-            VStack {
-                ForEach(manager.movies) {movie in
-                    MovieView(movie : movie).padding()
+        Group { //Header
+            HStack {
+                Text("Popular").bold()
+                Spacer()
+            }
+            Rectangle().frame(width : .infinity, height : 1)
+        }
+        Group { //Movie Information
+            ScrollView {
+                VStack {
+                    ForEach(manager.movies) {movie in
+                        MovieView(movie : movie).padding()
+                    }
                 }
             }
-            Spacer()
+        }
+    }
+}
+
+struct HomePage_Previews: PreviewProvider {
+    
+    static let manager = Manager()
+    
+    @State static var loaded = false
+    
+    static var previews: some View {
+        
+        VStack {
+            if(loaded) {
+                HomePage().environmentObject(AppState()).environmentObject(manager)
+            }
+        }.onAppear() {
+            manager.load() {
+                loaded = true
+            }
         }
     }
 }
